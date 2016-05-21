3e2dbfd4d1b319321563dd2b763556f3bcc6b0b6
IlPaco <IlPaco@IlPaco-PC>
2016-05-21 18:12:32 -0300

    21/5/2016

--- Obligatorio/src/Dominio/Obligatorio.java
+++ Obligatorio/src/Dominio/Obligatorio.java
@@ -5,6 +5,8 @@
  */
 package Dominio;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author IlPaco
@@ -14,8 +16,57 @@ public class Obligatorio {
     /**
      * @param args the command line arguments
      */
+    private ArrayList<Usuario> listaUsuarios;
+    private ArrayList<Producto> listaProductos;
+    private ArrayList<Compra> listaCompras;
+    private ArrayList<GastosFijo> listaGastosFijos;
+    private ArrayList<String> listaGastosMensuales;
+
+    public ArrayList<Usuario> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    public void setListaUsuarios(ArrayList<Usuario> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
+    public ArrayList<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(ArrayList<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public ArrayList<Compra> getListaCompras() {
+        return listaCompras;
+    }
+
+    public void setListaCompras(ArrayList<Compra> listaCompras) {
+        this.listaCompras = listaCompras;
+    }
+
+    public ArrayList<GastosFijo> getListaGastosFijos() {
+        return listaGastosFijos;
+    }
+
+    public void setListaGastosFijos(ArrayList<GastosFijo> listaGastosFijos) {
+        this.listaGastosFijos = listaGastosFijos;
+    }
+
+    public ArrayList<String> getListaGastosMensuales() {
+        return listaGastosMensuales;
+    }
+
+    public void setListaGastosMensuales(ArrayList<String> listaGastosMensuales) {
+        this.listaGastosMensuales = listaGastosMensuales;
+    }
+    
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
     
 }
